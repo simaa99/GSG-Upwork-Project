ecb9ef779564422578bf06a8771a4d3612b13013
simaa99 <semaamosa99@gmail.com>
2023-09-22 00:58:46 +0300

    create page loading

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { Backdrop, CircularProgress } from '@mui/material';
+import React, { FC } from 'react';
 
-const loading = () => {
+interface IndexProps {
+  open: boolean;
+  handleClose: () => void;
+  color?: "primary" | "secondary" | "inherit" | undefined;
+}
+
+const Index: FC<IndexProps> = ({ open, handleClose, color }) => {
   return (
-    <div>
-     loading.....
-    </div>
+    <Backdrop
+      sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+      onClick={handleClose}
+    >
+      <CircularProgress color={color || "inherit"} />
+    </Backdrop>
   );
 };
-export default loading;
+
+export default Index;
