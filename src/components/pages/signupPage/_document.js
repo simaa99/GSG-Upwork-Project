c710246aac9c61caf2ef63bc1e67fbf7d2182b76
simaa99 <semaamosa99@gmail.com>
2023-09-23 00:16:23 +0300

    create style component sign up page

---
+++ src/components/pages/signupPage/_document.js
@@ -0,0 +1,31 @@
+import { Alert, Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+export const InputContainer = styled(Box)({
+    display: 'flex',
+    alignItems: "center",
+    gap: 20,
+    flexDirection: "column",
+    width: "100%"
+})
+export const SignUpContainer = styled("dev")({
+    marginTop: "5rem",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  });
+
+  export default function InputAlert({ err }) {
+    return (
+      <>
+        {err && (
+          <Alert
+            sx={{ width: "100%", border: "none" , color:'#d32f2f'  , fontSize:'0.7rem'}}
+            severity="error"
+            children={err}
+          />
+        )}
+      </>
+    );
+  }
