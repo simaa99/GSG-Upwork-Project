c3fb4fe8eaf7e2a62346ae79fc9f0665929955ce
simaa99 <semaamosa99@gmail.com>
2023-09-23 22:30:48 +0300

    change position for layout

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,26 @@
+import Header from "@/components/molecules/Header/Header";
+import Footer from "@/components/molecules/Footer/Footer";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "upwork",
+  description: "create upwork app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+}
