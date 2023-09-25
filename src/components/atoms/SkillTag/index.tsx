c1ac05f0ccc2154e5df9b2bc9e8d15b0ded3f648
simaa99 <semaamosa99@gmail.com>
2023-09-25 10:39:24 +0300

    add skills tag component

---
+++ src/components/atoms/SkillTag/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Tag } from './_document';
+
+interface Props {
+
+ 
+}
+
+const Index: React.FC<Props> = (props) => {
+  return <Tag {...props} />;
+};
+
+export default Index;
