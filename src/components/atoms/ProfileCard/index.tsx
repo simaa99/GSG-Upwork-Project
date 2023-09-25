e583bdd5d92515a8ead771912cb84d2c89d66511
simaa99 <semaamosa99@gmail.com>
2023-09-25 10:40:37 +0300

    profile card component

---
+++ src/components/atoms/ProfileCard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Avatar from '@/components/molecules/Avatar';
+import Link from 'next/link';
+
+const Index: React.FC = () => {
+  return (
+    <Box padding={3} display={"flex"} alignItems="center" flexDirection={"column"}>
+      <Avatar imgSize={"65px"} />
+      <Typography variant='h6' mt={"20px"}>
+        <Link
+          href="/profile"
+        >
+          Simaa S.
+        </Link>
+      </Typography>
+      <Typography variant='body1'>Front End Develober</Typography>
+    </Box>
+  );
+};
+
+export default Index;
